81ddfd7ad3ed5d38522e2a55ef7de86a7278ece2
Expand Estates <expandestates@gmail.com>
2024-04-14 12:14:22 -0400

    Refactored footer code and manipulate header functionality

---
+++ theme/js/EEscript.js
@@ -0,0 +1,41 @@
+/**
+ * WEBSITE: https://themefisher.com
+ * TWITTER: https://twitter.com/themefisher
+ * FACEBOOK: https://www.facebook.com/themefisher
+ * GITHUB: https://github.com/themefisher/
+ */
+
+(function ($) {
+  "use strict";
+
+  $(function () {
+    $("#footer").load("footer.html");
+  });
+
+  // $("#EEHeader").load("header.html", function(responseTxt, statusTxt, xhr) {
+  //   if (statusTxt == "success") {
+  //     // Manipulate the loaded content directly
+  //     $("#header1").addClass("active");
+  //     console.log("Class added to #header1:", $("#header1").attr("class"));
+  //   } else if (statusTxt == "error") {
+  //     console.error("Error loading header.html:", xhr.status + ": " + xhr.statusText);
+  //   }
+  // });
+  window.fetchAndManipulateHeaders = function (page) {
+    $("#EEHeaders").load("header.html", function (responseTxt, statusTxt, xhr) {
+      if (statusTxt == "success") {
+        // Manipulate the loaded content directly based on the 'page' parameter
+        $("#" + page).addClass("active");
+        console.log(
+          "Class added to #" + page + ":",
+          $("#" + page).attr("class")
+        );
+      } else if (statusTxt == "error") {
+        console.error(
+          "Error loading header.html:",
+          xhr.status + ": " + xhr.statusText
+        );
+      }
+    });
+  };
+})(jQuery);
