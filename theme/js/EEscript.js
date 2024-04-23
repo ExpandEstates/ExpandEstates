f252d2d66898f245b5a25bbfa1a1669ae62bfc8c
Expand Estates <expandestates@gmail.com>
2024-04-23 01:21:59 -0400

    Add Changes to FAQ Page

--- theme/js/EEscript.js
+++ theme/js/EEscript.js
@@ -29,10 +29,11 @@
     $("#EE-page-header").load(
       "/theme/templates/EE-page-header.html",
       function (responseTxt, statusTxt, xhr) {
+        var ref = $(this);
         if (statusTxt == "success") {
           // Manipulate the loaded content directly based on the 'page' parameter
-          $("#" + pageHeader).text(pageHeaderText);
-          $("#" + pageDirectory).text(pageDirectoryText);
+          ref.find("#" + pageHeader).text(pageHeaderText);
+          ref.find("#" + pageDirectory).text(pageDirectoryText);
         } else if (statusTxt == "error") {
           console.error(
             "Error loading header.html:",
@@ -47,9 +48,39 @@
     $("#EE-headers").load(
       "/theme/templates/EE-header.html",
       function (responseTxt, statusTxt, xhr) {
+        var refPage = $(this);
         if (statusTxt == "success") {
           // Manipulate the loaded content directly based on the 'page' parameter
-          $("#" + page).addClass("active");
+          refPage.find("#" + page).addClass("active");
+        } else if (statusTxt == "error") {
+          console.error(
+            "Error loading header.html:",
+            xhr.status + ": " + xhr.statusText
+          );
+        }
+      }
+    );
+  };
+
+  window.fetchAndManipulateFAQCard = function (
+    pageHeader,
+    pageHeaderText,
+    collapseNbr
+  ) {
+    $("#EE-faq-card").load(
+      "/theme/templates/EE-faq-card.html",
+      function (responseTxt, statusTxt, xhr) {
+        var faqCard = $(this); // Reference to the loaded FAQ card content
+
+        if (statusTxt == "success") {
+          // Manipulate the loaded content directly based on the 'page' parameter
+          faqCard.find("." + pageHeader).attr("id", pageHeaderText);
+          faqCard.find(".collapse").attr("id", collapseNbr);
+          faqCard.find(".btn").attr("data-target", "#" + collapseNbr);
+          faqCard.find(".btn").attr("aria-controls", collapseNbr);
+          faqCard.find(".collapse").attr("aria-labelledby", pageHeaderText);
+
+          console.log(pageHeader);
         } else if (statusTxt == "error") {
           console.error(
             "Error loading header.html:",
