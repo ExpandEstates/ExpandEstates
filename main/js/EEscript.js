03e4b9e1f2a8c31b216ca37278e3311415acdf90
Expand Estates <expandestates@gmail.com>
2024-04-28 13:02:52 -0400

    change /expandestates/ to /expandesttates

--- main/js/EEscript.js
+++ main/js/EEscript.js
@@ -124,10 +124,9 @@
   }
 
   if (window.location.hostname === "expandestates.github.io") {
-    // Add '/ExpandEstates' to the beginning of all links
     document.querySelectorAll("a[href]").forEach(function (anchor) {
       var href = anchor.getAttribute("href");
-      anchor.setAttribute("href", "/ExpandEstates/" + href);
+      anchor.setAttribute("href", "/ExpandEstates" + href);
     });
   }
 })(jQuery);
