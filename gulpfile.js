58d48ab499e21c7d9ca94419bbab1e8b41352b90
Expand Estates <expandestates@gmail.com>
2024-04-30 14:40:42 -0400

    Implement Basic Email Functionality

--- gulpfile.js
+++ gulpfile.js
@@ -178,6 +178,30 @@ gulp.task("others:build", function () {
   return gulp.src(path.src.others).pipe(gulp.dest(path.build.dirDev));
 });
 
+var mail = require("gulp-mail");
+
+var smtpInfo = {
+  service: "gmail",
+  auth: {
+    user: "expandestates@gmail.com",
+    pass: "cazl hvkj culg uzko",
+  },
+  // host: "localhost",
+  // secureConnection: true,
+  // port: 465,
+};
+gulp.task("mail:build", function (done) {
+  gulp.src("source/partials/blocks/contact.htm").pipe(
+    mail({
+      subject: "SurprisesJimmy!?",
+      to: ["jzjimmy3@gmail.com"],
+      from: "expandestates@gmail.com",
+      smtp: smtpInfo,
+    })
+  );
+  done();
+});
+
 // Clean Build Folder
 gulp.task("clean", function (cb) {
   rimraf("./theme", cb);
@@ -193,6 +217,7 @@ gulp.task("watch:build", function () {
   gulp.watch(path.src.plugins, gulp.series("plugins:build"));
   gulp.watch(path.src.templates, gulp.series("templates:build"));
   gulp.watch(path.src.pages, gulp.series("pages:build"));
+  gulp.watch("source/partials/blocks/contact.htm", gulp.series("mail:build"));
 });
 
 // Dev Task
@@ -208,6 +233,7 @@ gulp.task(
     "others:build",
     "templates:build",
     "pages:build",
+    "mail:build",
     gulp.parallel("watch:build", function () {
       bs.init({
         server: {
